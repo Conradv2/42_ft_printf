4f34df7fb2ad35faeccb86aa58edb16e99b3e691
Conradv2 <marek226@interia.pl>
2025-01-28 04:00:02 +0100

    norminette check

--- ft_c_case.c
+++ ft_c_case.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 17:27:51 by kkruszyn          #+#    #+#             */
-/*   Updated: 2025/01/26 12:41:02 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/28 03:59:46 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	ft_c_case(int *i, va_list ap)
 {
 	int	count;
-	
+
 	count = 0;
 	ft_putchar_fd(va_arg(ap, int), 1);
 	count++;
