ec82756c521d65e218353d9884e13f6894654145
Conradv2 <marek226@interia.pl>
2025-01-29 01:53:44 +0100

    minor changes

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:30:35 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/26 14:44:50 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/29 01:27:47 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 # include "stdarg.h"
 # include "stdio.h"
 
-int		ft_printf(const char *string, ...);
-int		ft_string_iterate(const char *string, va_list ap);
-int		ft_specifier_check(const char *string, int *i, va_list ap);
-int		ft_percent_case(int *i);
-int		ft_s_case(int *i, va_list ap);
-int		ft_normal_case(const char *string, int *i);
-int		ft_c_case(int *i, va_list ap);
-int		ft_d_i_case(int *i, va_list ap);
-int     ft_digit_counter(int digit);
-int		ft_u_case(int *i, va_list ap);
+int			ft_printf(const char *string, ...);
+int			ft_string_iterate(const char *string, va_list ap);
+int			ft_specifier_check(const char *string, int *i, va_list ap);
+int			ft_percent_case(int *i);
+int			ft_s_case(int *i, va_list ap);
+int			ft_normal_case(const char *string, int *i);
+int			ft_c_case(int *i, va_list ap);
+int			ft_d_i_case(int *i, va_list ap);
+size_t		ft_digit_counter(int digit);
+int			ft_u_case(int *i, va_list ap);
 #endif
