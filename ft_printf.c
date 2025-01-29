94ec8ed877fa686152d60de03bf4c8f88b2a735d
Conradv2 <marek226@interia.pl>
2025-01-29 02:30:51 +0100

    changed main func in ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 19:01:54 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/26 13:50:25 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/29 02:18:06 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,42 @@ int	ft_printf(const char *string, ...)
 
 int	main(void)
 {
-	char	c;
-	char	s[] = "siemano";
-	char	*j = "AAAAAAAAA";
+	// char	c;
+	// char	s[] = "siemano";
+	// char	*j = "AAAAAAAAA";
 	int		a;
 	int		b;
-	int		d = 0xf;
+	int		d = 10;
 	int		i = 012;
 	int		hex = 0x12;
+	int		my_int = -2147483647;
+	//				-2147483648
+	//				 2147483647
+	//				 4294967295
 
-	c = 'e';
-	a = ft_printf("%i %%cze %% is: %c %s teś%scik", d, c, s, j);
+	//			-2147483648 and	2147483648 doesnt count properly
+
+
+	//			za duzo nalicza mi %d w przypadku wyjscia poza zasieg inta
+	//			za duzo tak samo nalicza w przypadku %u i max unsigned int value
+	// 			bardzo mozliwe ze musze napisac nowego ft_putnbr_fd, ktory by dzialal tylko na unsigned int lub size_t
+	//c = 'e';
+	a = ft_printf("siemano\n%dcotasadsadsa", my_int);
 	printf("\n");
-	b = printf("%i %%cze %% is: %c %s teś%scik", d, c, s, j);
+	b = printf("siemano\n%dcotasadsadsa", my_int);
+	//b = printf("%d %i %% cze %% is: %c %s teś %s cik", my_int, d, c, s, j);
 	printf("\nft_printf = %d\nprintf = %d\n", a, b);
 	printf("%%d = %d\n", d);
 	printf("%%i = %i\n", i);
 	printf("%%i(hex) = %i\n", hex);
+	//ft_putnbr_fd(my_int, 1);
+	// char	string[] = " implementacji ";
+	// char	znak = 's';
+	// int		test;
+	// int		og_test;
+	// test = ft_printf("Siemano test własnej%sfunkcji printf %%%c, wartość zmiennej a to %d", string, znak, d);
+	// printf("\n");
+	// og_test = printf("Siemano test własnej%sfunkcji printf %%%c, wartość zmiennej a to %d", string, znak, d);
+	// printf("\n%d\n%d\n", test, og_test);
 	return (0);
 }
