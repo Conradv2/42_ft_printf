522c918e2de69ca589290aa72dca06d836189661
Conradv2 <marek226@interia.pl>
2025-01-29 01:49:44 +0100

    added special case handling for min int value, causing the %d format to not counting the string properly

--- ft_digit_counter.c
+++ ft_digit_counter.c
@@ -6,20 +6,27 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 13:47:09 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/26 14:29:20 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/29 01:48:34 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 #include "libft.h"
 
-int	ft_digit_counter(int digit)
+size_t	ft_digit_counter(int digit)
 {
 	int	count;
 
 	count = 0;
+	if (digit == -2147483648)
+		return (11);
 	if (digit == 0)
-	 return (count + 1);
+		return (count + 1);
+	if (digit < 0)
+	{
+		count = 1;
+		digit *= -1;
+	}
 	while (digit > 0)
 	{
 		digit = digit / 10;
@@ -27,3 +34,4 @@ int	ft_digit_counter(int digit)
 	}
 	return (count);
 }
+//cases for max int and max unsigned int
