e0d47c6f093935974d3a468a290f187b89d7e49e
Conradv2 <marek226@interia.pl>
2025-01-29 02:24:30 +0100

    ft_u_case in progres

--- ft_u_case.c
+++ ft_u_case.c
@@ -6,24 +6,37 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 14:43:45 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/26 14:45:34 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/29 02:17:55 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-int ft_u_case(int *i, va_list ap)
+void	ft_u_putnbr_fd(unsigned int n, int fd)
 {
-	int	count;
-	int	ap_value;
+	char	_n;
+
+	if (n <= 9)
+	{
+		_n = n + '0';
+		write (fd, &_n, 1);
+	}
+	else
+	{
+		ft_u_putnbr_fd(n / 10, fd);
+		ft_u_putnbr_fd(n % 10, fd);
+	}
+}
+
+int	ft_u_case(int *i, va_list ap)
+{
+	unsigned int	ap_value;
+	int				count;
 
 	ap_value = va_arg(ap, int);
 	count = ft_digit_counter(ap_value);
-	ft_putnbr_fd(ap_value, 1);
-	if (count < 10)
-		(*i) += 2;
-	else
-		(*i) += 3;
+	ft_u_putnbr_fd(ap_value, 1);
+	(*i) += 2;
 	return (count);
 }
