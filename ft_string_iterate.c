b313255ed5cd4a2b676fac7e8414f2cb7016f2cb
Conradv2 <marek226@interia.pl>
2025-01-29 02:23:03 +0100

    major logic change, removed else statement to increment i, no reason to have it in code

--- ft_string_iterate.c
+++ ft_string_iterate.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 17:28:25 by kkruszyn          #+#    #+#             */
-/*   Updated: 2025/01/26 12:35:58 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/29 02:22:26 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,11 @@ int	ft_string_iterate(const char *string, va_list ap)
 			ft_count += ft_normal_case(string, &i);
 		else if (string[i] == '%')
 			ft_count += ft_specifier_check(string, &i, ap);
-		else
-			i++;
+		else if (string[i] == '\\')
+			ft_count += ft_escape_sequences(string, &i);
 	}
 	return (ft_count);
 }
+
+		// else
+		// 	i++;
