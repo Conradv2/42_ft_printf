52c1afd6b9fcacfb40179de8320fc3e90c60b2e2
Conradv2 <marek226@interia.pl>
2025-01-30 01:36:16 +0100

    changed ft_digit_counter.c --> ft_u_digit_counter.c that handles unsigned int case

--- ft_u_case.c
+++ ft_u_case.c
@@ -6,28 +6,12 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 14:43:45 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/29 02:17:55 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/30 01:32:02 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
-
-void	ft_u_putnbr_fd(unsigned int n, int fd)
-{
-	char	_n;
-
-	if (n <= 9)
-	{
-		_n = n + '0';
-		write (fd, &_n, 1);
-	}
-	else
-	{
-		ft_u_putnbr_fd(n / 10, fd);
-		ft_u_putnbr_fd(n % 10, fd);
-	}
-}
 
 int	ft_u_case(int *i, va_list ap)
 {
@@ -35,7 +19,7 @@ int	ft_u_case(int *i, va_list ap)
 	int				count;
 
 	ap_value = va_arg(ap, int);
-	count = ft_digit_counter(ap_value);
+	count = ft_u_digit_counter(ap_value);
 	ft_u_putnbr_fd(ap_value, 1);
 	(*i) += 2;
 	return (count);
