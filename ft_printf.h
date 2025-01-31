711ef79fc62fbbad03b1837678c6f4e1d6758e8a
Conradv2 <marek226@interia.pl>
2025-01-31 02:44:23 +0100

    ft_x_case func declaration

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:30:35 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/30 01:32:53 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/31 02:43:57 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,10 @@ int			ft_s_case(int *i, va_list ap);
 int			ft_normal_case(const char *string, int *i);
 int			ft_c_case(int *i, va_list ap);
 int			ft_d_i_case(int *i, va_list ap);
+int			ft_x_case(int *i, va_list ap);
 size_t		ft_digit_counter(int digit);
 size_t		ft_u_digit_counter(unsigned int digit);
+int			ft_x_case(int *i, va_list ap);
 void		ft_u_putnbr_fd(unsigned int n, int fd);
 int			ft_u_case(int *i, va_list ap);
 int			ft_escape_sequences(const char *string, int *i);
