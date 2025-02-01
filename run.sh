14020a6a92476b2f3a245f11a2a16ad0c0476927
Konrad Kruszynski <kkruszyn@c1r4s3.42warsaw.pl>
2025-02-01 15:52:26 +0100

    readable run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,23 @@
-cc ft_c_case.c ft_d_i_case.c ft_digit_counter.c ft_escape_sequences.c ft_normal_case.c ft_percent_case.c ft_printf.c ft_putchar_fd.c ft_putnbr_fd.c ft_putstr_fd.c ft_s_case.c ft_specifier_check.c ft_string_iterate.c ft_strlen.c ft_u_case.c ft_u_digit_counter.c ft_u_putnbr_fd.c ft_x_case.c -Wall -Werror -Wextra -std=c99 -g
+cc ft_c_case.c \
+    ft_d_i_case.c \
+    ft_digit_counter.c \
+    ft_escape_sequences.c \
+    ft_normal_case.c \
+    ft_percent_case.c \
+    ft_printf.c \
+    ft_putchar_fd.c \
+    ft_putnbr_fd.c \
+    ft_putstr_fd.c \
+    ft_s_case.c \
+    ft_specifier_check.c \
+    ft_string_iterate.c \
+    ft_strlen.c \
+    ft_u_case.c \
+    ft_u_digit_counter.c \
+    ft_u_putnbr_fd.c \
+    ft_x_case.c \
+    -Wall -Werror -Wextra -std=c99 -g
+#cc ft_printf.c ft_escape_sequences.c ft_u_case.c ft_string_iterate.c ft_normal_case.c ft_specifier_check.c ft_c_case.c ft_s_case.c ft_putstr_fd.c ft_percent_case.c ft_d_i_case.c ft_digit_counter.c ft_strlen.c ft_putchar_fd.c ft_x_case.c ft_putnbr_fd.c ft_u_digit_counter.c ft_u_putnbr_fd.c 
+#cc ft_printf.c ft_escape_sequences.c ft_u_case.c ft_string_iterate.c ft_normal_case.c ft_specifier_check.c ft_c_case.c ft_s_case.c ft_putstr_fd.c ft_percent_case.c ft_d_i_case.c ft_digit_counter.c ft_strlen.c ft_putchar_fd.c ft_x_case.c ft_putnbr_fd.c ft_u_digit_counter.c ft_u_putnbr_fd.c 
 ./a.out
+rm ./a.out
