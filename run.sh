7258aa15315e36b3dd285feb846a974437fde9af
Konrad Kruszynski <kkruszyn@c1r4s3.42warsaw.pl>
2025-02-01 16:41:14 +0100

    new functions

--- run.sh
+++ run.sh
@@ -8,13 +8,14 @@ cc \
     ft_c_case.c \
     ft_d_i_case.c \
     ft_x_case.c \
+    ft_swap_array.c \
+    ft_print_array.c \
     ft_digit_counter.c \
     ft_u_digit_counter.c \
     ft_u_putnbr_fd.c \
     ft_putnbr_fd.c \
     ft_u_case.c \
     ft_escape_sequences.c \
-    ft_x_case_utils.c \
     ft_putchar_fd.c \
     ft_putstr_fd.c \
     ft_strlen.c \
