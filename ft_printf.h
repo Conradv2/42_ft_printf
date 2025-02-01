4779acd787a9fd6d3fc668fd764e609c55e6a3b9
Konrad Kruszynski <kkruszyn@c1r4s3.42warsaw.pl>
2025-02-01 15:01:18 +0100

    changed ft_digit_counter and ft_x_case size_t to int

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:30:35 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/31 02:43:57 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/02/01 15:00:20 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "stdarg.h"
 # include "stdio.h"
+# include "limits.h"
 
 int			ft_printf(const char *string, ...);
 int			ft_string_iterate(const char *string, va_list ap);
@@ -24,10 +25,9 @@ int			ft_s_case(int *i, va_list ap);
 int			ft_normal_case(const char *string, int *i);
 int			ft_c_case(int *i, va_list ap);
 int			ft_d_i_case(int *i, va_list ap);
-int			ft_x_case(int *i, va_list ap);
-size_t		ft_digit_counter(int digit);
+int     	ft_x_case(int *i, va_list ap);
+int         ft_digit_counter(int digit);
 size_t		ft_u_digit_counter(unsigned int digit);
-int			ft_x_case(int *i, va_list ap);
 void		ft_u_putnbr_fd(unsigned int n, int fd);
 int			ft_u_case(int *i, va_list ap);
 int			ft_escape_sequences(const char *string, int *i);
