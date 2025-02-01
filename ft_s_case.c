6c9ee60ec57745ffadfbf6a7b8cf799c4c9e7baa
Konrad Kruszynski <kkruszyn@c1r4s3.42warsaw.pl>
2025-02-01 12:44:19 +0100

    handled NULL as a %s specifier

--- ft_s_case.c
+++ ft_s_case.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_s_case.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 17:27:03 by kkruszyn          #+#    #+#             */
-/*   Updated: 2025/01/26 13:00:30 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/02/01 12:43:31 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
+
 
 int	ft_s_case(int *i, va_list ap)
 {
@@ -20,8 +21,16 @@ int	ft_s_case(int *i, va_list ap)
 
 	count = 0;
 	str = va_arg(ap, char *);
-	ft_putstr_fd(str, 1);
+	if (str == NULL)
+		str = "(null)";
 	count += ft_strlen(str);
+	ft_putstr_fd(str, 1);
 	(*i) += 2;
 	return (count);
 }
+
+	// if (str == NULL)
+	// {
+	// 	(*i) += 2;	
+	// 	return (0);
+	// }
