4b84e40db04db58e8764034cbda070275695a30f
Konrad Kruszynski <kkruszyn@c1r4s3.42warsaw.pl>
2025-02-01 16:14:38 +0100

    added a few functions working with ft_x_case.c

---
+++ ft_x_case_utils.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_x_case_utils.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 15:43:41 by kkruszyn          #+#    #+#             */
+/*   Updated: 2025/02/01 15:45:22 by kkruszyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_X_CASE_UTILS_H
+# define FT_X_CASE_UTILS_H
+
+# include "stdarg.h"
+# include "stdio.h"
+# include "limits.h"
+
+void	ft_swap_array(char *_x_array);
+void	ft_print_array(const char *_x_array);
+
+#endif
