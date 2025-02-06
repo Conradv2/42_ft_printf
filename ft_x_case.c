448db58adc55651e23216b70579cd191a3e80de9
Konrad Kruszynski <kkruszyn@c3r7s3.42warsaw.pl>
2025-02-06 14:39:47 +0100

    shortened this function by creating new one -> ft_fill_array

--- ft_x_case.c
+++ ft_x_case.c
@@ -6,7 +6,7 @@
 /*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 01:40:25 by conradv2          #+#    #+#             */
-/*   Updated: 2025/02/06 14:16:47 by kkruszyn         ###   ########.fr       */
+/*   Updated: 2025/02/06 14:38:27 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,45 +19,17 @@ int	ft_x_case(int *i, va_list ap)
 	unsigned int			tmp;
 	int						count;
 	char					*x_array;
-	int						j;
 
 	ap_value = va_arg(ap, unsigned int);
 	tmp = ap_value;
 	count = 0;
-	j = 0;
 	if (ap_value == 0)
-	{
 		ft_putnbr_fd(0, 1);
-		(*i) += 2;
-		return (1);
-	}
 	count = ft_hex_digit_counter(ap_value);
 	x_array = (char *)malloc((count + 1) * sizeof(char));
 	if (x_array == NULL)
 		return (0);
-	while (ap_value != 0)
-	{
-		if (ap_value % 16 == 10)
-			x_array[j] = 'a';
-		else if (ap_value % 16 == 11)
-			x_array[j] = 'b';
-		else if (ap_value % 16 == 12)
-			x_array[j] = 'c';
-		else if (ap_value % 16 == 13)
-			x_array[j] = 'd';
-		else if (ap_value % 16 == 14)
-			x_array[j] = 'e';
-		else if (ap_value % 16 == 15)
-			x_array[j] = 'f';
-		else
-		{
-			x_array[j] = (ap_value % 16) + '0';
-		}
-		ap_value /= 16;
-		j++;
-	}
-	x_array[j] = '\0';
-	j = 0;
+	ft_fill_array(ap_value, x_array);
 	ft_reverse_array(x_array);
 	ft_print_array(x_array);
 	(*i) += 2;
@@ -103,9 +75,12 @@ int	ft_x_case(int *i, va_list ap)
 	// 							 -----> swap_arr(x_arr) --------
 	//											               |
 	//									x_arr[] = {6, 4} <------
-i pass a number as a integer, for example 255, then i want to divide it by 16 and then divide the number that was just divided
-and so on but store the % remainder in a array, so it would be two while loops, first would count how many times it is dividing and take this number
-as a representative of how much space to allocate so if 255 takes 2 divides then it should allocate 2 + 1 space (cause of NULL) and at the end i should 
+i pass a number as a integer, for example 255, then i want to divide it by 16
+and then divide the number that was just divided
+and so on but store the % remainder in a array, so it would be two while loops, 
+first would count how many times it is dividing and take this number
+as a representative of how much space to allocate so if 255 takes 2 divides then 
+it should allocate 2 + 1 space (cause of NULL) and at the end i should 
 reverse the string and print it to get the value im looking for
 
 
