bccb3d06d44c6a4577abfca09b208790d186fe76
Konrad Kruszynski <kkruszyn@c3r7s3.42warsaw.pl>
2025-02-06 13:58:42 +0100

    added new function and made it more readable

--- run.sh
+++ run.sh
@@ -1,24 +1,24 @@
 cc \
-    ft_printf.c \
-    ft_string_iterate.c \
-    ft_specifier_check.c \
-    ft_percent_case.c \
-    ft_s_case.c \
-    ft_normal_case.c \
     ft_c_case.c \
     ft_d_i_case.c \
-    ft_x_case.c \
-    ft_swap_array.c \
-    ft_print_array.c \
     ft_digit_counter.c \
+    ft_escape_sequences.c \
+    ft_hex_digit_counter.c \
+    ft_normal_case.c \
+    ft_percent_case.c \
+    ft_print_array.c \
+    ft_printf.c \
+    ft_putchar_fd.c \
+    ft_putnbr_fd.c \
+    ft_putstr_fd.c \
+    ft_s_case.c \
+    ft_specifier_check.c \
+    ft_string_iterate.c \
+    ft_strlen.c \
+    ft_swap_array.c \
+    ft_u_case.c \
     ft_u_digit_counter.c \
     ft_u_putnbr_fd.c \
-    ft_putnbr_fd.c \
-    ft_u_case.c \
-    ft_escape_sequences.c \
-    ft_putchar_fd.c \
-    ft_putstr_fd.c \
-    ft_strlen.c \
+    ft_x_case.c \
     -Wall -Werror -Wextra -std=c99 -g
 ./a.out
-rm ./a.out
