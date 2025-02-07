0c9ff6a799a8874d1d4bb8362e73f4a84651f5f9
Conradv2 <marek226@interia.pl>
2025-02-08 00:40:04 +0100

    pre-alpha version of ft_p_case --> need to test and separate functions

--- ft_p_case.c
+++ ft_p_case.c
@@ -6,12 +6,26 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 18:32:34 by conradv2          #+#    #+#             */
-/*   Updated: 2025/02/07 23:43:51 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/02/08 00:36:15 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
+
+int ft_hex_x_case(size_t ap_value, int count)
+{
+	char	*x_array;
+
+	x_array = (char *)malloc((count + 1) * sizeof(char));
+	if (x_array == NULL)
+		return (0);
+	ft_l_fill_array(ap_value, x_array);
+	ft_reverse_array(x_array);
+	ft_putstr_fd(x_array, 1);
+	free(x_array);
+	return (count);
+}
 
 int	ft_p_case(int *i, va_list ap)
 {
@@ -23,11 +37,13 @@ int	ft_p_case(int *i, va_list ap)
 	if (ap_value == NULL)
 	{
 		ft_putstr_fd("(nil)", 1);
+		(*i) += 2;
 		return (5);
 	}
 	ft_putstr_fd("0x", 1);
-	count += 2;
 	count += ft_hex_digit_counter((size_t)ap_value);
+	ft_hex_x_case((size_t)ap_value, count);
+	count += 2;
 	(*i) += 2;
 	return (count);
 }
