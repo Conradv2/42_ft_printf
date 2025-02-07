0b2a297740c0386c7b7497e55df22b874376cefc
Conradv2 <marek226@interia.pl>
2025-02-08 00:59:40 +0100

    removed libft.h

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,14 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 17:07:18 by conradv2          #+#    #+#             */
-/*   Updated: 2025/02/06 11:47:40 by kkruszyn         ###   ########.fr       */
+/*   Updated: 2025/02/08 00:59:26 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
 #include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
