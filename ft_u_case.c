b7967dfb180ac83e9a316338d1a4eaae982c93b8
Conradv2 <marek226@interia.pl>
2025-02-08 01:02:11 +0100

    removed libft.h

--- ft_u_case.c
+++ ft_u_case.c
@@ -6,12 +6,11 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 14:43:45 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/30 01:32:02 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/02/08 01:02:01 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include "libft.h"
 
 int	ft_u_case(int *i, va_list ap)
 {
